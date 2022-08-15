c503e9968d4ad2d594d83bb1fc9fa65693bd26d8
Kitty <very.gay.kitten@gmail.com>
2022-08-15 16:39:43 +0100

    Refactored the actix transporter API.

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,10 @@ pub mod error;
 ///
 /// This module has a specialized implementation for `ButtplugConnectorTransport`
 /// that can be used with actix web websockets.
-#[cfg(feature = "actix-ws-transport")]
+#[cfg(feature = "actix-web")]
 pub mod transport;
 
-use std::sync::Arc;
+use std::{future::Future, sync::Arc};
 
 use delegate::delegate;
 use error::Result;
@@ -96,7 +96,11 @@ where
 	A: StreamHandler<ButtplugClientEvent>,
 {
 	/// Start a new actor that uses the ButtplugContext with a given connector.
-	pub async fn start_with_connector<Con>(act: A, name: &str, connector: Con) -> Result<Addr<A>>
+	pub async fn start_with_connector<Con>(
+		act: A,
+		name: &str,
+		connector: Con,
+	) -> Result<Addr<A>, ButtplugClientError>
 	where
 		Con: ButtplugConnector<ButtplugCurrentSpecClientMessage, ButtplugCurrentSpecServerMessage>
 			+ 'static,
@@ -132,29 +136,35 @@ where
 	///     res
 	/// }
 	/// ```
-	#[cfg(feature = "actix-ws-transport")]
-	pub async fn start_with_actix_ws_transport<S>(
+	#[cfg(feature = "actix-web")]
+	pub async fn start_with_actix_ws_transport<S, F, R>(
 		actor: A,
-		name: &str,
+		name: impl AsRef<str> + 'static + Send,
 		req: &actix_web::HttpRequest,
 		stream: S,
-	) -> Result<(Addr<A>, HttpResponse)>
+		on_connection: F,
+	) -> Result<HttpResponse>
 	where
-		A: Actor<Context = ButtplugContext<A>>,
+		A: Actor<Context = ButtplugContext<A>> + Send,
 		S: Stream<Item = Result<Bytes, PayloadError>> + 'static,
+		F: FnOnce(Result<Addr<A>, ButtplugClientError>) -> R + 'static + Send,
+		R: Future<Output = ()> + 'static + Send,
 	{
 		let (trans, res) = ButtplugActixWebsocketTransport::new(req, stream)?;
 
-		let conn: ButtplugRemoteConnector<
-			ButtplugActixWebsocketTransport,
-			ButtplugClientJSONSerializer,
-			_,
-			_,
-		> = ButtplugRemoteConnector::new(trans);
+		tokio::task::spawn(async move {
+			let conn: ButtplugRemoteConnector<
+				ButtplugActixWebsocketTransport,
+				ButtplugClientJSONSerializer,
+				_,
+				_,
+			> = ButtplugRemoteConnector::new(trans);
+			let name = name.as_ref();
+			let addr = Self::start_with_connector(actor, name, conn).await;
+			on_connection(addr).await
+		});
 
-		let addr = Self::start_with_connector(actor, name, conn).await?;
-
-		Ok((addr, res))
+		Ok(res)
 	}
 }
 
